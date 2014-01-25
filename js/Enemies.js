f4d89b26909acf806b83209ea0a6b554ba5b8d97
BranimirE <brani.espinoza@gmail.com>
2014-01-25 16:05:34 -0400

    Ahora si terminado Enemies

--- js/Enemies.js
+++ js/Enemies.js
@@ -1,9 +1,11 @@
 enchant();
 
-var player;
-
 var Enemy = Class.create(Sprite, {
-    initialize:  function(x, y, size, type, velocity, evil){
+    initialize:  function(x, y, size, type, velocityX, velocityY, evil){
+
+        //var rata = new Enemy(10, 0, 16, 'cara', 1, 1, true);
+        //var rata3 = new Enemy(10, 0, 16, 'cara', 0.5, 1.1, true);
+
         Sprite.call(this, size, size); // Modificar por tamaño real de sprit
 
         this.size = size;
@@ -11,8 +13,8 @@ var Enemy = Class.create(Sprite, {
         this.image = game.assets[type + '.png']; // Crear la imagen y adicionarlo a los preload
         this.x = x;
         this.y = y;
-        this.xx = velocity;//Velocidad en x
-        this.yy = 1;
+        this.xx = velocityX;//Velocidad en x
+        this.yy = velocityY;
         this.frame = [40,40,41,41];
         this.dir = 0; // direction 0: right 1: left
         //stage.addChild(this);
@@ -34,10 +36,16 @@ var Enemy = Class.create(Sprite, {
         this.x += this.xx;
 
 
-        if(this.evil && this.intersect(player) && this.x % 3 == 0)
+        if(this.evil && this.within(player) && this.x % 3 == 0){
             player.opacity = 0.5;
-        else
-            player.opacity = 1;
+            player.damaged = true;
+        } else {
+            if(!player.damaged) {
+                player.opacity = 1;
+            } else {
+                player.damaged = false;
+            }
+        }
     }
 });
 
