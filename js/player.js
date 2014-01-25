0bc4d0920d4a8fa08aebb8dc6bdd771b4ce60fe0
jhtan <jhtan@jhtan.com>
2014-01-25 17:26:16 -0400

    Pepe transitions.

--- js/player.js
+++ js/player.js
@@ -9,7 +9,7 @@ var Player = Class.create(Sprite, {
         this.yy = 0;
         this.opacity = 1;
         this.damage=0;
-        this.speed = 3;
+        this.speed = 7;
         this.jump = false;
         this.jumpcnt=0;
         this.frame = 0;
@@ -21,11 +21,10 @@ var Player = Class.create(Sprite, {
 
     },
     onenterframe: function() {
-
         if(game.input.up){
             if(this.jump){
                 this.jump=false;
-                this.jumpcnt=11;
+                this.jumpcnt=6; // 11
                 //game.assets["pi31.wav"].clone().play();
             }
         }
@@ -33,7 +32,11 @@ var Player = Class.create(Sprite, {
         if(this.jumpcnt){
             this.jumpcnt--;
             if(game.input.up){
-                this.yy= -4.6;
+                //this.yy= -4.6;
+                this.yy= -7;
+
+		// Animation.
+		this.frame = 1;
             }else{
                 this.jumpcnt=0;
             }
@@ -43,7 +46,8 @@ var Player = Class.create(Sprite, {
 
         if (map.hitTest(player.x+8,player.y+32) === false){
             this.jump = false;
-            this.yy += 0.6;
+            //this.yy += 0.6;
+	    this.yy += 2;
         }else{
             this.jump = true;
             this.yy =0;
@@ -57,10 +61,25 @@ var Player = Class.create(Sprite, {
             this.jumpcnt=0;this.yy+=3;
         }
 
+	// Moving to the left.
+        if (game.input.right) {
+	    this.xx = this.speed;
 
-        if (game.input.left )this.xx = -this.speed;
-        if (game.input.right )this.xx = this.speed;
+	    // Animation.
+	    if(this.frame != 0 && this.frame != 2) this.frame = 0;
+	    else if(this.frame == 0) this.frame = 2;
+	    else if(this.frame == 2) this.frame = 0;
+	}
 
+	// Moving to the right.
+        if (game.input.left) {
+	    this.xx = -this.speed;
+
+	    // Animation.
+	    if(this.frame != 5 && this.frame != 7) this.frame = 7;
+	    else if(this.frame == 5) this.frame = 7;
+	    else if(this.frame == 7) this.frame = 5;
+	}
 
         if(game.input.left === false && game.input.right === false){
             if(this.xx>0)this.xx-=this.speed;
@@ -71,17 +90,17 @@ var Player = Class.create(Sprite, {
 
 
         if (map.hitTest(player.x,player.y+8 + 8)){
-            console.log('lugar 1');
+            //console.log('lugar 1');
             this.x = Math.floor(this.x / 16) * 16 + 14;
         }
 
         if (map.hitTest(player.x+16+16,player.y+8)){
-            console.log('lugar 2');
+            //console.log('lugar 2');
             this.x = Math.floor(this.x / (16+16)) * (16+16);
         }
 
         if (map.hitTest(player.x+8,player.y+32)){
-            console.log('lugar 3');
+            //console.log('lugar 3');
             this.y = Math.floor(this.y / 32) * 32;
             /*
             if(this.xx!=0){
@@ -111,9 +130,9 @@ var Player = Class.create(Sprite, {
             }
 */
 
-        }else{
-            if(this.frame<21)this.frame=21;
-            if(this.frame>21)this.frame=27;
+//        }else{
+//            if(this.frame<21)this.frame=21;
+//            if(this.frame>21)this.frame=27;
         }
 
 /*        if(this.bulletcnt)this.bulletcnt--;
