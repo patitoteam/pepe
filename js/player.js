90d308213786fccc068edcaa5ba9012e9982cda5
Victor Aguilar C <victor@jvacx.com>
2014-01-25 23:00:22 -0400

    endgame method

--- js/player.js
+++ js/player.js
@@ -108,19 +108,28 @@ var Player = Class.create(Sprite, {
 	    }
         }
 
-	// Animations.
-	if(game.input.right) {
-	    // Walking to the right.
-	    if(this.frame != 0 && this.frame != 1  && this.frame != 2) this.frame = 0;
-	    else if(this.frame == 0) this.frame = 1;
-	    else if(this.frame == 1) this.frame = 2;
-	    else if(this.frame == 2) this.frame = 0;
-	} else if(game.input.left) {
-	    // Walking to the left.
-	    if(this.frame != 5 && this.frame != 6 && this.frame != 7) this.frame = 7;
-	    else if(this.frame == 5) this.frame = 6;
-	    else if(this.frame == 6) this.frame = 7;
-	    else if(this.frame == 7) this.frame = 5;
-	}
+    	// Animations.
+    	if(game.input.right) {
+    	    // Walking to the right.
+    	    if(this.frame != 0 && this.frame != 1  && this.frame != 2) this.frame = 0;
+    	    else if(this.frame == 0) this.frame = 1;
+    	    else if(this.frame == 1) this.frame = 2;
+    	    else if(this.frame == 2) this.frame = 0;
+    	} else if(game.input.left) {
+    	    // Walking to the left.
+    	    if(this.frame != 5 && this.frame != 6 && this.frame != 7) this.frame = 7;
+    	    else if(this.frame == 5) this.frame = 6;
+    	    else if(this.frame == 6) this.frame = 7;
+    	    else if(this.frame == 7) this.frame = 5;
+    	}
+        if(this.life<=0) {
+            console.log("finished");
+            game.endGame("GAME OVER");
+        }
+
+        if(this.x>= game.height) {
+            console.log("finished");
+            game.endGame("GAME OVER");
+        }
     }
 });
