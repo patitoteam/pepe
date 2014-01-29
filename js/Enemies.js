7ed9a477d71491b592d1f9d94f1271fdcec10535
BranimirE <brani.espinoza@gmail.com>
2014-01-29 14:46:17 -0400

    more bugs, bugs, bugs, bugs, :P seems Windows

--- js/Enemies.js
+++ js/Enemies.js
@@ -1,11 +1,6 @@
 enchant();
-
 var Enemy = Class.create(Sprite, {
-    initialize:  function(x, y, size, type, velocityX, velocityY, evil){
-
-        //var rata = new Enemy(10, 0, 16, 'cara', 1, 1, true);
-        //var rata3 = new Enemy(10, 0, 16, 'cara', 0.5, 1.1, true);
-
+    initialize:  function(x, y, size, type, velocityX, velocityY, evil, fotogramas){
         Sprite.call(this, size, size); // Modificar por tamaño real de sprit
 
         this.size = size;
@@ -13,35 +8,74 @@ var Enemy = Class.create(Sprite, {
         this.image = game.assets[type + '.png']; // Crear la imagen y adicionarlo a los preload
         this.x = x;
         this.y = y;
+        this.bckX = x;
+        this.bckY = y;
         this.xx = velocityX;//Velocidad en x
         this.yy = velocityY;
-        this.frame = [40,40,41,41];
+        this.frame = [0,0,1,1];
         this.dir = 0; // direction 0: right 1: left
-        //stage.addChild(this);
-
+        this.fotogramas = fotogramas;
+        if(fotogramas){
+            this.derecha = [];
+            this.izquierda = [];
+            for(var i = 0; i < fotogramas; i++){
+                this.derecha.push(i);
+                this.izquierda.push(i + fotogramas);
+            }
+            this.frame = this.derecha;
+        }
     },
     onenterframe: function(){
-        if(this.dir == RIGHT && map.hitTest(this.x + this.size, this.y)){
+
+        /*Volver al inicio cuando muere*/
+        if(this.y >= game.height){
+            this.x = this.bckX;
+            this.y = this.bckY;
+        }
+
+        if(this.dir == RIGHT && map.hitTest(this.x + this.size, this.y + this.size / 2)){
             this.xx = -this.xx;
             this.dir = LEFT;
-            this.frame= [42,42,43,43];
-        }else if(this.dir == LEFT && map.hitTest(this.x, this.y)){
+            if(this.fotogramas)
+                this.frame = this.izquierda;
+            else
+                this.frame= [2,2,3,3];
+
+        }else if(this.dir == LEFT && map.hitTest(this.x, this.y + this.size / 2)){
             this.xx = -this.xx;
             this.dir = RIGHT;
-            this.frame = [40,40,41,41];
+            if(this.fotogramas)
+                this.frame = this.derecha;
+            else
+                this.frame = [0,0,1,1];
         }
+
         if(!map.hitTest(this.x, this.y + this.size) && !map.hitTest(this.x + this.size, this.y + this.size))
             this.y += this.yy;
 
         this.x += this.xx;
 
 
-        if(this.evil && this.within(player) && ( this.x % 5 == 0 || this.x % 7 == 0) ){
-            player.opacity = 0.5;
+        if(this.evil && this.intersect(player)){
+            if( this.x % 5 == 0 || this.x % 7 == 0)
+                player.opacity = 0.5;
+
+            if(game.frame % 6 == 0){
+                player.life--;
+                obj = game.assets["music/danio_1.wav"];
+                obj.volume = 1;
+                obj.play();
+            }
+
+
+            // Animación.
+            if(player.xx > 0)
+                player.frame = 3;
+
+            if(player.xx < 0)
+                player.frame = 4;
+
             player.damaged = true;
-            obj = game.assets["music/danio_2.wav"].clone();
-            obj.volume = 1;
-            obj.play();
         } else {
             if(!player.damaged) {
                 player.opacity = 1;
@@ -49,9 +83,11 @@ var Enemy = Class.create(Sprite, {
                 player.damaged = false;
             }
         }
+
+        //Ver la posicion del personaje en tamaños de 32 x 32
+        //info.innerText = 'X: '+ player.x/32 + ' , Y: ' + player.y/32;
     }
 });
 
-/******/
 var RIGHT = 0;
 var LEFT = 1;
--- js/main2.js
+++ js/main2.js
@@ -1,7 +1,8 @@
-
 enchant();
+
 var info = document.getElementById('information');
 info.innerHTML = '';
+
 var game = new Game(640, 320);
 
 game.preload(
@@ -43,110 +44,8 @@ game.preload(
 
 game.fps = 15;
 
-var DIR_LEFT = 0;
-var DIR_RIGHT = 1;
-var DIR_UP = 2;
-var DIR_DOWN = 3;
-/******/
-var gameOver = false;
-var falling = true;
 var player;
-var level = 1;
-var Enemy = Class.create(Sprite, {
-    initialize:  function(x, y, size, type, velocityX, velocityY, evil, fotogramas){
-        Sprite.call(this, size, size); // Modificar por tamaño real de sprit
-
-        this.size = size;
-        this.evil = evil;
-        this.image = game.assets[type + '.png']; // Crear la imagen y adicionarlo a los preload
-        this.x = x;
-        this.y = y;
-        this.bckX = x;
-        this.bckY = y;
-        this.xx = velocityX;//Velocidad en x
-        this.yy = velocityY;
-        this.frame = [0,0,1,1];
-        this.dir = 0; // direction 0: right 1: left
-        this.fotogramas = fotogramas;
-        if(fotogramas){
-            this.derecha = [];
-            this.izquierda = [];
-            for(var i = 0; i < fotogramas; i++){
-                this.derecha.push(i);
-                this.izquierda.push(i + fotogramas);
-            }
-            this.frame = this.derecha;
-        }
-    },
-    onenterframe: function(){
-
-        /*Volver al inicio cuando muere*/
-        if(this.y >= game.height){
-            this.x = this.bckX;
-            this.y = this.bckY;
-        }
-
-        if(this.dir == RIGHT && map.hitTest(this.x + this.size, this.y + this.size / 2)){
-            this.xx = -this.xx;
-            this.dir = LEFT;
-            if(this.fotogramas)
-                this.frame = this.izquierda;
-            else
-                this.frame= [2,2,3,3];
-
-        }else if(this.dir == LEFT && map.hitTest(this.x, this.y + this.size / 2)){
-            this.xx = -this.xx;
-            this.dir = RIGHT;
-            if(this.fotogramas)
-                this.frame = this.derecha;
-            else
-                this.frame = [0,0,1,1];
-        }
-
-        if(!map.hitTest(this.x, this.y + this.size) && !map.hitTest(this.x + this.size, this.y + this.size))
-            this.y += this.yy;
-
-        this.x += this.xx;
-
-
-        if(this.evil && this.intersect(player)){
-            if( this.x % 5 == 0 || this.x % 7 == 0)
-                player.opacity = 0.5;
-
-            if(game.frame % 6 == 0){
-                player.life--;
-                obj = game.assets["music/danio_1.wav"];
-                obj.volume = 1;
-                obj.play();
-            }
-
-
-            // Animación.
-            if(player.xx > 0)
-                player.frame = 3;
-
-            if(player.xx < 0)
-                player.frame = 4;
-
-            player.damaged = true;
-        } else {
-            if(!player.damaged) {
-                player.opacity = 1;
-            } else {
-                player.damaged = false;
-            }
-        }
-
-        //Ver la posicion del personaje en tamaños de 32 x 32
-        //info.innerText = 'X: '+ player.x/32 + ' , Y: ' + player.y/32;
-    }
-});
-
-/******/
-var RIGHT = 0;
-var LEFT = 1;
-
-// -----------------------------------------------------------------------------
+var self;
 
 window.onload = function() {
     game.onload = function() {
@@ -197,11 +96,6 @@ window.onload = function() {
          life.x = 50;
          life.y = 50;
          // stage.addChild(life);
-         });
-
-         // Health Bar
-         window.healthbar = new HealthBar({
-         stage  : stage
          });
          */
 
